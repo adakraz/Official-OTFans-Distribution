8e2070b48acafb93716361eaf66faded6015cc10
Lars Naslund <lasse_sv91@hotmail.com>
2012-06-21 07:07:00 +0200

    * Map update

--- data/exp_stages.lua
+++ data/exp_stages.lua
@@ -1,4 +1,4 @@
---if you want to enable exp_stages, make sure that you have experience_stages = true at your config.lua
+-- if you want to enable exp_stages, make sure that you have experience_stages = true at your config.lua
 
 stages = {
 	{minLevel = 1, maxLevel = 40, multiplier = 60},
