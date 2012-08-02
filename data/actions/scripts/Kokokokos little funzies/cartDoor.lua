9196f238764f07e560da76403c1118002c7d005a
Lars Naslund <lasse_sv91@hotmail.com>
2012-08-02 22:05:19 +0200

    * Removed all data folders except the map

--- data/actions/scripts/Kokokokos little funzies/cartDoor.lua
+++
@@ -1,6 +0,0 @@
-function onUse(cid, item, fromPosition, itemEx, toPosition)
-
-    doSendMagicEffect(getCreaturePosition(cid), 2)
-    doPlayerSendCancel(cid, "The door will not move.")
-        return 1
-end
