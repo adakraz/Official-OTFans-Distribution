f2f4032412c4a4e04c66fe59ca2445d95a8f80c3
Lars Naslund <lasse_sv91@hotmail.com>
2012-08-16 15:11:39 +0200

    * http://otfans.net/project.php?issueid=1056 * Map update done to rook, minor.

---
+++ data/scripts/npcs/ood/mainland/postman.lua
@@ -0,0 +1,32 @@
+local Postman = NPC:new("Postman")
+ 
+-- TODO: change outfit
+Postman.outfit = { 
+	type = 128, 
+	head = 116, 
+	body = 79, 
+	legs = 117, 
+	feet = 76
+}
+ 
+Postman.greeting = {"Welcome to the post office!"}
+Postman.bored	 = {"If you need help with letters or parcels, just ask me. I can explain everything."}
+Postman.farewell = {"Hey, send a letter to your friend now and then. Keep in touch, you know."}
+Postman.runoff 	 = {"How rude!"}
+ 
+Benjamin.idleTimeout = 30
+Benjamin.listenRadius = 4
+Benjamin.walkRadius = 3
+ 
+--[[
+Postman.dialog = {
+	{"job|occupation|offer", "I sell letters, parcels, labels..."};
+	-- TODO: quest
+ }
+]]--
+
+Postman.trade = {
+	{'parcel', 	 	 id=2595, buy=10};
+	{'letter',  	 id=2597, buy=5};
+	{'label', 	 	 id=2599, buy=1};
+}
