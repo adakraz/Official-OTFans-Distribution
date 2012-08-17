039e646147dea39948d2900040a6f5db96fab3d2
Lars Naslund <lasse_sv91@hotmail.com>
2012-08-17 19:54:28 +0200

    * http://otfans.net/project.php?issueid=1055

--- data/scripts/npcs/ood/mainland/jewelery.lua
+++ data/scripts/npcs/ood/mainland/jewelery.lua
@@ -28,7 +28,7 @@ Jeweler.dialog = {
 -- The items are sorted by sell/buy value
 Jeweler.trade = {
  
-	-- Buyable jewels
+-- Buyable jewels
 	{'golden amulet', 	 id=2130, buy=6600};
 	--{'golden globet', 	 id=????, buy=5000};
 	{'ruby necklace', 	 id=2133, buy=3560};
@@ -44,26 +44,26 @@ Jeweler.trade = {
 	{'white pearl', 	 id=2143, buy=320};
  
 	-- Sellable jewels
-	--{'gold ingot', 	 			id=????, buy=5000};
-	{'giant shimmering pearl',  id=7632, buy=3000};
-	{'giant shimmeting pearl', 	id=7633, buy=3000};
-	--{'blue crystal shard',  	id=????, buy=1500};
-	--{'green crystal shard',	    id=????, buy=1500};
-	--{'violet crystal shard',    id=????, buy=1500};
-	--{'cyan crystal fragment', 	id=????, buy=800};
-	--{'greent crystal fragment', id=????, buy=800};
-	--{'red crystal fragment',    id=????, buy=800};
-	--{'blue crystal splinter', 	id=????, buy=400};
-	--{'brown crystal splinter', 	id=????, buy=400};
-	--{'green crystal splinter',  id=????, buy=400};
-	{'small diamond', 	 		id=2145, buy=300};
-	{'black pearl',  	 		id=2144, buy=280};
-	{'small emerald', 	 		id=2149, buy=250};
-	{'small ruby', 	 	 		id=2147, buy=250};
-	{'small sapphire', 	 		id=2146, buy=250};
-	{'small amethyst', 	 		id=2150, buy=200};
-	{'small topaz', 	 	    id=9970, buy=200};
-	{'white pearl', 	 		id=2143, buy=160};
-	{'wedding ring', 	 		id=2121, buy=100};
+	--{'gold ingot', 	 			id=????, sell=5000};
+	{'giant shimmering pearl',  id=7632, sell=3000};
+	{'giant shimmeting pearl', 	id=7633, sell=3000};
+	--{'blue crystal shard',  	id=????, sell=1500};
+	--{'green crystal shard',	    id=????, sell=1500};
+	--{'violet crystal shard',    id=????, sell=1500};
+	--{'cyan crystal fragment', 	id=????, sell=800};
+	--{'greent crystal fragment', id=????, sell=800};
+	--{'red crystal fragment',    id=????, sell=800};
+	--{'blue crystal splinter', 	id=????, sell=400};
+	--{'brown crystal splinter', 	id=????, sell=400};
+	--{'green crystal splinter',  id=????, sell=400};
+	{'small diamond', 	 		id=2145, sell=300};
+	{'black pearl',  	 		id=2144, sell=280};
+	{'small emerald', 	 		id=2149, sell=250};
+	{'small ruby', 	 	 		id=2147, sell=250};
+	{'small sapphire', 	 		id=2146, sell=250};
+	{'small amethyst', 	 		id=2150, sell=200};
+	{'small topaz', 	 	    id=9970, sell=200};
+	{'white pearl', 	 		id=2143, sell=160};
+	{'wedding ring', 	 		id=2121, sell=100};
  
 }
